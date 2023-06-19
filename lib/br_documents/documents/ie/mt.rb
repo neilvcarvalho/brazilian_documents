8bea0f1c2b3aa213857b9de92cf988b7f3e78505
Felipe Felix <felipe.felix@maino.com.br>
2023-06-19 15:27:00 -0300

    fix: digito 14 valido

--- lib/br_documents/documents/ie/mt.rb
+++ lib/br_documents/documents/ie/mt.rb
@@ -1,14 +1,23 @@
 # http://www.sintegra.gov.br/Cad_Estados/cad_MT.html
 module BRDocuments
   class IE::MT < IE::Base
-
     set_verify_digits_weights first: %w(9 8 7 6 5 4 3 2)
 
-    set_format_regexp %r{^(13)[-.]?(\d{3})[-.]?(\d{3})[\/-]?(\d{1})}
+    set_format_regexp %r{^(\d{2})[-.]?(\d{3})[-.]?(\d{3})[\/-]?(\d{1})}
 
     set_pretty_format_mask %(%s.%s.%s-%s)
 
-    set_fixed_digits [1, 3]
+    set_fixed_digits [13, 14]
 
+    def self.valid_fixed_digits?(number)
+      number = new(number).normalize
+      current = number[0..1].join.to_i
+
+      const_get('FIXED_INITIAL_NUMBERS').member?(current)
+    end
+
+    def self.fixed_digits
+      super.sample.to_s.split(//).map(&:to_i)
+    end
   end
 end
